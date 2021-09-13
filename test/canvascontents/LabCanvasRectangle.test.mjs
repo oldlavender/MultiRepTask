bb8c4169ba70738b59cc0dc8a25b1cf0a3daf7a3
Bruno Moreira-Guedes <thbmatrix@gmail.com>
2021-09-13 20:32:55 -0300

    - Added a LabCanvasRectangle class and its test

---
+++ test/canvascontents/LabCanvasRectangle.test.mjs
@@ -0,0 +1,34 @@
+import { LabCanvasRectangle } from "../../lib/canvascontent/LabCanvasRectangle.mjs";
+
+var lcr = [];
+
+describe(
+    "",
+    ()=>{
+        test(
+            "LabCanvasRectangle constructor must set passed values or ".concat(
+                "defaults when parameters are not passed, except for ",
+                "the mandatory parameters"
+            ),
+            ()=>{
+                lcr.push(new LabCanvasRectangle(130, 100));
+                lcr.push(new LabCanvasRectangle(
+                    160, 120, -300, 10, 90, 'orange', 'just-a-random-rectangle'
+                ));
+                expect(lcr[0].width).toBe(130);
+                expect(lcr[0].height).toBe(100);
+                expect(lcr[0].left).toEqual(0);
+                expect(lcr[0].top).toEqual(0);
+                expect(lcr[0].angle).toEqual(0);
+                expect(lcr[0].fill).toBe('black');
+                expect(lcr[0].id).toBe('unamed-rectangle');
+
+                expect(lcr[1].width).toBe(160);
+                expect(lcr[1].height).toBe(120);
+                expect(lcr[1].left).toEqual(-300);
+                expect(lcr[1].top).toEqual(10);
+                expect(lcr[1].angle).toEqual(90);
+                expect(lcr[1].fill).toBe('orange');
+                expect(lcr[1].id).toBe('just-a-random-rectangle');
+            }
+        );
