df75647072e030744c4be675086805ba35a9b93c
Bruno Moreira-Guedes <thbmatrix@gmail.com>
2021-09-17 01:09:50 -0300

    -Updated tests for LabCanvasSquare

--- test/canvascontents/LabCanvasSquare.test.mjs
+++ test/canvascontents/LabCanvasSquare.test.mjs
@@ -1,5 +1,4 @@
-import { LabCanvasSquare } from "../../lib/canvascontent/LabCanvasSquare.mjs";
-import { rect_defaults } from "../../lib/canvascontent/LabCanvasRectangle.mjs";
+import { LabCanvasSquare, square_defaults } from "../../lib/canvascontent/LabCanvasSquare.mjs";
 
 var lcr = [];
 
@@ -7,30 +6,30 @@ describe(
     "LabCanvasSquare",
     ()=>{
         test(
-            "LabCanvasRectangle constructor must set passed values or ".concat(
+            "LabCanvasSquare constructor must set passed values or ".concat(
                 "defaults when parameters are not passed, except for ",
-                "the mandatory parameters"
+                "the mandatory parameters."
             ),
             ()=>{
                 lcr.push(new LabCanvasSquare(80));
+                expect(lcr[0].width).toBe(80);
+                expect(lcr[0].height).toBe(80);
+                expect(lcr[0].dimensions).toBe(80);
+                expect(lcr[0].left).toBe(square_defaults.left);
+                expect(lcr[0].top).toBe(square_defaults.top);
+                expect(lcr[0].angle).toBe(square_defaults.angle);
+                expect(lcr[0].fill).toBe(square_defaults.fill);
+                expect(lcr[0].id).toBe(square_defaults.id);
+
                 lcr.push(new LabCanvasSquare(
                     120, -300, 10, 90, 'orange', 'just-a-random-square'
                 ));
-                expect(lcr[0].width).toBe(80);
-                expect(lcr[0].height).toBe(80);
-                expect(lcr[0].dimension).toBe(80);
-                expect(lcr[0].left).toEqual(rect_defaults.left);
-                expect(lcr[0].top).toEqual(rect_defaults.top);
-                expect(lcr[0].angle).toEqual(rect_defaults.angle);
-                expect(lcr[0].fill).toBe(rect_defaults.fill);
-                expect(lcr[0].id).toBe(rect_defaults.id);
-
                 expect(lcr[1].width).toBe(120);
                 expect(lcr[1].height).toBe(120);
-                expect(lcr[1].dimension).toBe(120);
-                expect(lcr[1].left).toEqual(-300);
-                expect(lcr[1].top).toEqual(10);
-                expect(lcr[1].angle).toEqual(90);
+                expect(lcr[1].dimensions).toBe(120);
+                expect(lcr[1].left).toBe(-300);
+                expect(lcr[1].top).toBe(10);
+                expect(lcr[1].angle).toBe(90);
                 expect(lcr[1].fill).toBe('orange');
                 expect(lcr[1].id).toBe('just-a-random-square');
             }
