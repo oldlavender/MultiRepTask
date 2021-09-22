538d47c982ebb4270cad3a450559d72be318ca05
Bruno Moreira-Guedes <thbmatrix@gmail.com>
2021-09-22 16:43:46 -0300

    -Test for LabCanvasText class

---
+++ test/canvascontents/LabCanvasText.test.mjs
@@ -0,0 +1,44 @@
+import { LabCanvasText, text_defaults } from "../../lib/canvascontent/LabCanvasText.mjs";
+
+var lcr = [];
+
+describe(
+    "LabCanvasText",
+    ()=>{
+        test(
+            "LabCanvasSquareText constructor must set passed values or ".concat(
+                "defaults when parameters are not passed, except for ",
+                "the mandatory parameters."
+            ),
+            ()=>{
+                /*
+                    .text
+                    .fontSize (optional, default 32)
+                    .fontStyle (optional, default 'normal')
+                    .fontWeight (optional, default 'normal')
+                    .fontFamily (optional, default 'sans-serif')
+                    .textAlign (optional, default 'center')
+                    .lineHeight (optional, default 1.16)
+                */
+                const t1 = 'hello!', t2 = 'world';
+                lcr.push(new LabCanvasText(t1));
+                expect(lcr[0].text).toEqual(t1);
+                expect(lcr[0].fontSize).toEqual(text_defaults.fontSize);
+                expect(lcr[0].fontStyle).toEqual(text_defaults.fontStyle);
+                expect(lcr[0].fontWeight).toEqual(text_defaults.fontWeight);
+                expect(lcr[0].fontFamily).toEqual(text_defaults.fontFamily);
+                expect(lcr[0].textAlign).toEqual(text_defaults.textAlign);
+                expect(lcr[0].lineHeight).toEqual(text_defaults.lineHeight);
+
+                lcr.push(new LabCanvasText(t2, 'left', 16, 1.16, 'bold', 'italic', 'serif'));
+                expect(lcr[1].text).toEqual(t2);
+                expect(lcr[1].fontSize).toEqual(16);
+                expect(lcr[1].fontStyle).toEqual('italic');
+                expect(lcr[1].fontWeight).toEqual('bold');
+                expect(lcr[1].fontFamily).toEqual('serif');
+                expect(lcr[1].textAlign).toEqual('left');
+                expect(lcr[1].lineHeight).toEqual(1.16);
+            }
+        );
+    }
+);
